e0a603116ba3865da954d2f6fe260c7690b55426
Koma | Developer <77354592+TheRealKoma@users.noreply.github.com>
2023-05-20 14:01:19 +0200

    host_lock.lua

--- resources/[system]/sessionmanager/server/host_lock.lua
+++ resources/[system]/sessionmanager/server/host_lock.lua
@@ -1,9 +1,18 @@
 -- whitelist c2s events
-RegisterServerEvent('hostingSession')
-RegisterServerEvent('hostedSession')
+---@type table<string, number>
+local serverEventsWhitelist = {
+    hostingSession = 1,
+    hostedSession = 1
+}
+
+--- Registriert ein Event, das vom Client zum Server gesendet werden kann.
+---@param eventName string Der Name des zu registrierenden Events.
+RegisterServerEvent(eventName)
 
 -- event handler for pre-session 'acquire'
+---@type number|nil
 local currentHosting
+---@type table<number, function>
 local hostReleaseCallbacks = {}
 
 -- TODO: add a timeout for the hosting lock to be held
@@ -25,7 +34,6 @@ AddEventHandler('hostingSession', function()
     if GetHostId() then
         if GetPlayerLastMsg(GetHostId()) < 1000 then
             TriggerClientEvent('sessionHostResult', source, 'conflict')
-
             return
         end
     end
@@ -64,6 +72,18 @@ AddEventHandler('hostedSession', function()
     end
 
     currentHosting = nil
+end)
+
+-- TODO: Add timeout for the host lock to be released automatically
+SetTimeout(60000, function()
+    if currentHosting then
+        -- Release the host lock if it's still held after 1 minute
+        for _, cb in ipairs(hostReleaseCallbacks) do
+            cb()
+        end
+
+        currentHosting = nil
+    end
 end)
 
 EnableEnhancedHostSupport(true)
